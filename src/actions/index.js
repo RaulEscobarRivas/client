fdb863603bcba38c837ac81127d7c1d303b32838
Raul Escobar <raulescobar13@gmail.com>
2017-02-19 22:20:13 -0300

    Writes action creator to do authenticated requests using jwt token

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,8 @@ import { browserHistory } from 'react-router';
 import {
     AUTH_USER,
     UNAUTH_USER,
-    AUTH_ERROR
+    AUTH_ERROR,
+    FETCH_MESSAGE
 } from './types';
 
 const API_URL = 'http://localhost:3090';
@@ -51,6 +52,20 @@ export function signupUser(email, password) {
             })
             .catch(error => {
                 dispatch(authError(error.response.data.error));
+            });
+    }
+}
+
+export function fetchMessage() {
+    return function(dispatch) {
+        axios.get(API_URL, {
+            headers: { authorization: localStorage.getItem('token') }
+        })
+            .then( response => {
+                dispatch({
+                    type: FETCH_MESSAGE,
+                    payload: response.data.message
+                })
             });
     }
 }

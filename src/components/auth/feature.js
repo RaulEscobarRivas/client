cacbb2ef20db185cfb43c0f881fe4403c9496466
Raul Escobar <raulescobar13@gmail.com>
2017-03-25 19:37:10 -0300

    Feature component renders a list of features

--- src/components/auth/feature.js
+++ src/components/auth/feature.js
@@ -7,8 +7,32 @@ class Feature extends Component {
         this.props.fetchMessage();
     }
 
+    renderLinks() {
+        return [
+                <li className="nav-item" key={1}>
+                    <div>{'Arquero'}</div>
+                </li>,
+                <li className="nav-item" key={2}>
+                    <div>{'Defensa'}</div>
+                </li>,
+                <li className="nav-item" key={3}>
+                    <div>{'Medio campo'}</div>
+                </li>,
+                <li className="nav-item" key={4}>
+                    <div>{'Delantera'}</div>
+                </li>
+            ];
+    }
+
     render () {
-        return <div>{this.props.message}</div>;
+        return (
+            <div>
+                {this.props.message}
+                <ul className="nav navbar-nav feature-list">
+                    {this.renderLinks()}
+                </ul>
+            </div>
+        );
     }
 }
 

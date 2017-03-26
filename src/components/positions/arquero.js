12f6d2fadf0f15f893ab8152b114cca1d1ba6242
Raul Escobar <raulescobar13@gmail.com>
2017-03-26 18:42:19 -0300

    Arquero component renders options to select

--- src/components/positions/arquero.js
+++ src/components/positions/arquero.js
@@ -3,13 +3,32 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
 class Arquero extends Component {
+    handleSelect(e) {
+        console.log(e.currentTarget.dataset.option);
+    }
+
+    renderOptions(options) {
+        return options.map( (option, index) =>
+            <div key={index} className="card card-block" data-option={option} onClick={this.handleSelect.bind(this)}>
+                <h4 className="card-title">{option}</h4>
+                <p className="card-text">{'Selecciona al jugador.'}</p>
+            </div>
+        );
+    }
+
     render() {
-        return <div>{'Arquero Component'}</div>;
+        return (
+            <div className="player-options">
+                {this.renderOptions(this.props.options)}
+            </div>
+        );
     }
 }
 
 function mapStateToProps(state) {
-    return {};
+    return {
+        options: ['Jugador 1', 'Jugador 2', 'Jugador 3', 'Jugador 4']
+    };
 }
 
 export default connect(mapStateToProps, actions)(Arquero);
